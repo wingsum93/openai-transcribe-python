d8d6e995ccaae4951d8a218c271e311d942d26ea
Eric Ho <wingsum.developer@gmail.com>
2023-11-10 17:56:40 +0800

    refractor to class, runnable

--- transcribe-video-cli.py
+++ transcribe-video-cli.py
@@ -6,25 +6,13 @@ import whisper
 import numpy as np
 from pathlib import Path
 from pytube import YouTube
-import transcribe_video
+from transcribe_video import VideoRecognizer
 
 def main(video_path, model_type,language):
-    # Check if the video_path is a YouTube URL
-    if 'youtube.com' in video_path:
-        # Download the video using pytube
-        print(f"Downloading video from {video_path}")
-        # ... Code to download the video ...
 
-    # Load the Whisper model
-    print(f"Loading Whisper model: {model_type}")
-    # ... Code to load and use the Whisper model ...
-    model = whisper.load_model(f'{model_type}')
-    result, video = transcribe_video.transcribe(video_path, transcribe_video.save_path, transcribe_video.filename,language=language, model_type=model_type)
-    sub = transcribe_video.convert_to_subtitle(result['segments'])
-    sub_transcribed = transcribe_video.save_subtitle(sub, transcribe_video.save_path, transcribe_video.filename+'-sub', format=format)
-    print(f"\n\nsubtitle is saved at {sub_transcribed}")
-    print("Transcription complete!")
-
+    vr1 = VideoRecognizer(video_path=video_path, language=language, model_type=model_type)
+    vr1.detectVideo()
+    
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Transcribe a video using OpenAI Whisper.')
     parser.add_argument('--video_path', type=str, required=True, help='The path to the video file or a YouTube URL.')
--- transcribe_video.py
+++ transcribe_video.py
@@ -7,103 +7,116 @@ from pytube import YouTube
 import random
 import string
 
-
-#@markdown If `video_path` is a YouTube link, the video will be downloaded at the `save_path`.
-#@param {type: 'string'}
-video_path = 'https://www.youtube.com/watch?v=WqMw0WABxOs' 
 #@markdown Choose a Whisper model. `base` is the fastest and uses the least amount of memory.
 #@param ["base", "small", "medium", "large"]
 model_type = 'medium'  
-#@markdown Video Language Code
-#@param {type: 'string'}
-video_lang = 'zh'   
-#@markdown Where to save the video and subtitle.
-#@param {type: 'string'}
-save_path = 'output'  
-save_path = Path(save_path)
-save_path.mkdir(exist_ok=True, parents=True)
-#@markdown What to name the saved video and subtitle.
-#@param {type: 'string'}
-filename = 'fishbrother'
-#@markdown Which format to save the subtitle in.
-#@param ["srt", "txt"]
-format = 'srt'
+   
 
 
+class VideoRecognizer:
+    def __init__(self,video_path,language,model_type='small',):
+        print("MyClass instance created")
+        #@markdown Where to save the video and subtitle.
+        #@param {type: 'string'}
+        self.save_path = 'output'  
+        self.save_path = Path(self.save_path)
+        self.save_path.mkdir(exist_ok=True, parents=True)
+        self.video_path=video_path
+        #@markdown What to name the saved video and subtitle.
+        #@param {type: 'string'}
+        self.filename = 'fishbrother'
+        #@markdown Video Language Code
+        #@param {type: 'string'}
+        self.video_lang = language
+        #@markdown Which format to save the subtitle in.
+        #@param ["srt", "txt"]
+        self.format = 'srt'
+    def detectVideo(self):
+        result, video, filename = self.transcribe()
+        sub = self.convert_to_subtitle(result['segments'])
+        sub_transcribed = self.save_subtitle(sub, self.save_path,filename+'-sub', format=format)
+        
 
-def get_video_from_youtube_url(url, save_path=None, filename=None):
-    yt = YouTube(url)
-    video_file = str(save_path/f'{filename}.mp4')
-    s = (yt.streams.filter(progressive=True, file_extension='mp4')
-         .order_by('resolution').asc().first()
-    )
-    s.download(filename=video_file)
-    return video_file
-
-
-def transcribe(video, save_path, filename,language, model_type='small'):
-    if video.startswith('http'):
-        print("Downloading Youtube Video\n")
-        random_filename=generate_random_filename()
-        video = get_video_from_youtube_url(video, save_path=save_path, filename=random_filename)
-    else :
-        print("Decode local video\n")
-    print(f"Decode language {language}.")
-    options = whisper.DecodingOptions(fp16=False, language=language)
-    model = whisper.load_model(model_type)
-    result = model.transcribe(video, **options.__dict__, verbose=False)
-    return result, video
-
-
-def segments_to_srt(segs):
-    text = []
-    for i,s in tqdm(enumerate(segs)):
-        text.append(str(i+1))
-
-        time_start = s['start']
-        hours, minutes, seconds = int(time_start/3600), (time_start/60) % 60, (time_start) % 60
-        timestamp_start = "%02d:%02d:%06.3f" % (hours, minutes, seconds)
-        timestamp_start = timestamp_start.replace('.',',')     
-        time_end = s['end']
-        hours, minutes, seconds = int(time_end/3600), (time_end/60) % 60, (time_end) % 60
-        timestamp_end = "%02d:%02d:%06.3f" % (hours, minutes, seconds)
-        timestamp_end = timestamp_end.replace('.',',')        
-        text.append(timestamp_start + " --> " + timestamp_end)
-
-        text.append(s['text'].strip() + "\n")
-            
-    return "\n".join(text)
+    def get_video_from_youtube_url(self,url, filename=None):
+        yt = YouTube(url)
+        video_file = str(self.save_path/f'{filename}.mp4')
+        s = (yt.streams.filter(progressive=True, file_extension='mp4')
+            .order_by('resolution').asc().first()
+        )
+        s.download(filename=video_file)
+        return video_file
 
 
-def convert_to_subtitle(segs):
-    if format == 'srt':
-        sub = segments_to_srt(segs)
-    elif format == 'txt':
-        sub = transcribed_text(segs)
-    else:
-        raise ValueError(f"format {format} is not supported!")
-    return sub
+    def transcribe(self):
+        if self.video_path.startswith('http') or 'youtube.com' in self.video_path:
+            random_filename=self.generate_random_filename()
+            print(f"Downloading Youtube Video {self.video_path}")
+            video = self.get_video_from_youtube_url(url=self.video_path,filename=random_filename)
+        else :
+            print("Decode local video\n")
+        print(f"Decode language {self.video_lang}.")
+        options = whisper.DecodingOptions(fp16=False, language=self.video_lang)
+        model = whisper.load_model(model_type)
+        result = model.transcribe(video, **options.__dict__, verbose=False)
+        if self.video_path.startswith('http') or 'youtube.com' in self.video_path:
+            return result, video, random_filename    
+        else :
+            return result, video, self.filename
+        
+
+
+    def segments_to_srt(segs):
+        text = []
+        for i,s in tqdm(enumerate(segs)):
+            text.append(str(i+1))
+
+            time_start = s['start']
+            hours, minutes, seconds = int(time_start/3600), (time_start/60) % 60, (time_start) % 60
+            timestamp_start = "%02d:%02d:%06.3f" % (hours, minutes, seconds)
+            timestamp_start = timestamp_start.replace('.',',')     
+            time_end = s['end']
+            hours, minutes, seconds = int(time_end/3600), (time_end/60) % 60, (time_end) % 60
+            timestamp_end = "%02d:%02d:%06.3f" % (hours, minutes, seconds)
+            timestamp_end = timestamp_end.replace('.',',')        
+            text.append(timestamp_start + " --> " + timestamp_end)
+
+            text.append(s['text'].strip() + "\n")
+                
+        return "\n".join(text)
+
+
+    def convert_to_subtitle(self,segs):
+        if format == 'srt':
+            sub = self.segments_to_srt(segs)
+        elif format == 'txt':
+            sub = self.transcribed_text(segs)
+        else:
+            raise ValueError(f"format {format} is not supported!")
+        return sub
+        
+
+    def save_subtitle(sub, save_path, filename, format='srt'):
+        srt_file = save_path/f'{filename}.{format}'
+        with open(srt_file, 'w') as f:
+            f.write(sub)
+        return srt_file
+
+
+    def transcribed_text(segs):
+        texts = [s['text'] for s in segs]
+        text = '\n'.join(texts)
+        return text
+
+    def generate_random_filename(prefix="youtube",extension=".mp4"):
+        """
+        Generate a random filename with a specified extension.
+
+        :param extension: File extension (default is .txt)
+        :return: Random filename as a string
+        """
+        letters = string.ascii_lowercase
+        filename = 'youtube'.join(random.choice(letters) for i in range(10))  # Generate a random string of length 10
+        return f"{prefix}_{filename}{extension}"
     
-
-def save_subtitle(sub, save_path, filename, format='srt'):
-    srt_file = save_path/f'{filename}.{format}'
-    with open(srt_file, 'w') as f:
-        f.write(sub)
-    return srt_file
-
-
-def transcribed_text(segs):
-    texts = [s['text'] for s in segs]
-    text = '\n'.join(texts)
-    return text
-
-def generate_random_filename(prefix="youtube",extension=".mp4"):
-    """
-    Generate a random filename with a specified extension.
-
-    :param extension: File extension (default is .txt)
-    :return: Random filename as a string
-    """
-    letters = string.ascii_lowercase
-    filename = 'youtube'.join(random.choice(letters) for i in range(10))  # Generate a random string of length 10
-    return f"{prefix}_{filename}{extension}"
+    def setOutputFormat(self,output_format): # srt / txt
+        self.format = output_format

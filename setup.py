e6c26809e34799473a8e0b06a9dee6c37709bf80
Eric Ho <wingsum.developer@gmail.com>
2023-11-21 04:53:17 +0800

    temp

--- setup.py
+++ setup.py
@@ -1,14 +1,29 @@
 from setuptools import setup, find_packages
 
+# Function to read the contents of requirements.txt and return as a list
+def read_requirements():
+    with open('requirements.txt') as file:
+        return file.read().splitlines()
+
 setup(
     name='subtitle_generator',
     version='0.0.1',
     packages=find_packages(exclude=['tests', 'tests.*']),
-    install_requires=[...],  # Dependencies listed in requirements.txt
+    install_requires=read_requirements(),
     entry_points={
         'console_scripts': [
-            'generate-subtitles=subtitle_generator.cli:main',
+            'stt=subtitle_generator.cli:main',
         ],
     },
-    # Other metadata like author, description, etc.
+    author='Eric Ho',
+    author_email='wingsum.developer@gmail.com',
+    description='A subtitle generation package using Whisper model. It also suport text translation using m2m100 facebook model',
+    license='MIT',
+    url='https://github.com/wingsum93/openai-transcribe-python',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ]
+
 )

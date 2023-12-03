06ad1b404697291060b7b0567eabb476f84ff8d9
Eric Ho <wingsum.developer@gmail.com>
2023-12-03 21:44:09 +0800

    add logger

--- subtitle_generator/video_processor.py
+++ subtitle_generator/video_processor.py
@@ -1,11 +1,13 @@
+from logger import Logger
 import ffmpeg
 import os
 
 class VideoProcessor:
-    def __init__(self, download_folder="downloads"):
+    def __init__(self, download_folder="downloads",logger:Logger = Logger()):
         self.download_folder = download_folder
         if not os.path.exists(download_folder):
             os.makedirs(download_folder)
+        self.logger = logger
 
     def convert_video_to_audio(self, video_path:str, audio_format="mp3"):
         print(f"VideoProcessor  video: {video_path}, format {audio_format}")
@@ -21,6 +23,7 @@ class VideoProcessor:
         if os.path.exists(audio_path):
             print(f"VideoProcessor audio already converted: {audio_path}")
             return audio_path
+        self.logger.logStartAction('convert_video_to_audio')
         try:
             (
                 ffmpeg
@@ -28,6 +31,7 @@ class VideoProcessor:
                 .output(audio_path, acodec='copy', vn=None)
                 .run(overwrite_output=True)
             )
+            self.logger.logEndAction('convert_video_to_audio')
             return audio_path
         except ffmpeg.Error as e:
             print(f"An error occurred during video-to-audio conversion: {e.stderr}")
